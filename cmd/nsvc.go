df5ef29385dd4274ba7709826786eca04ee1dcd3
mkusaka <hinoshita1992@gmail.com>
2022-04-24 18:57:19 +0900

    add version option

--- cmd/nsvc.go
+++ cmd/nsvc.go
@@ -8,9 +8,17 @@ import (
 	"github.com/Masterminds/semver"
 )
 
+var version string
+
 func main() {
 	versionType := flag.String("type", "patch", "bump target string.")
+	showVersion := flag.Bool("version", false, "print current version")
 	flag.Parse()
+
+	if *showVersion {
+		fmt.Println(version)
+		return
+	}
 
 	if len(flag.Args()) != 1 {
 		fmt.Println("invalid argument.")
@@ -18,7 +26,7 @@ func main() {
 	}
 	str := flag.Args()[0]
 
-	version, err := semver.NewVersion(str)
+	v, err := semver.NewVersion(str)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -33,11 +41,11 @@ func main() {
 	var incremented semver.Version
 	switch *versionType {
 	case "major":
-		incremented = version.IncMajor()
+		incremented = v.IncMajor()
 	case "minor":
-		incremented = version.IncMinor()
+		incremented = v.IncMinor()
 	case "patch":
-		incremented = version.IncPatch()
+		incremented = v.IncPatch()
 	default:
 		fmt.Printf("unsupported version type: %+v\n", *versionType)
 		os.Exit(1)
