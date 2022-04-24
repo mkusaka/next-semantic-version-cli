a7194b06f97177477c6324a82e1827921040a630
mkusaka <hinoshita1992@gmail.com>
2022-04-24 18:58:28 +0900

    v0.0.2

--- scripts/install.bash
+++ scripts/install.bash
@@ -2,7 +2,7 @@
 
 set -e -o pipefail
 
-version="${1:-0.0.1}"
+version="${1:-0.0.2}"
 
 if [[ ! "$version" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     echo "Given version '${version}' does not match to regex" '^[0-9]+\.[0-9]+\.[0-9]+$' >&2
