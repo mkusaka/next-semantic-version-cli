a90858ba0dc90f5ace0c3b1c249d025b4e06ee32
mkusaka <hinoshita1992@gmail.com>
2022-04-25 17:22:36 +0900

    update intall script

--- scripts/install.bash
+++ scripts/install.bash
@@ -2,14 +2,14 @@
 
 set -e -o pipefail
 
-version="${1:-$(curl https://api.github.com/repos/mkusaka/next-semantic-version-cli/releases/latest -s | jq .name -r)}"
+version="${1:-$(curl https://api.github.com/repos/mkusaka/next-semantic-version-cli/releases/latest -s | jq .name -r | sed -e s/^v//g)}"
 
-if [[ ! "$version" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-    echo "Given version '${version}' does not match to regex" '^v[0-9]+\.[0-9]+\.[0-9]+$' >&2
+if [[ ! "$version" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    echo "Given version '${version}' does not match to regex" '^[0-9]+\.[0-9]+\.[0-9]+$' >&2
     exit 1
 fi
 
-echo "Start downloading next-semantic-version-cli ${version}"
+echo "Start downloading next-semantic-version-cli v${version}"
 
 case "$OSTYPE" in
     linux-*)
@@ -40,12 +40,12 @@ esac
 echo "Detected OS=${os} ext=${ext} arch=${arch}"
 
 file="next-semantic-version-cli_${version}_${os}_${arch}.${ext}"
-url="https://github.com/mkusaka/next-semantic-version-cli/releases/download/${version}/${file}"
+url="https://github.com/mkusaka/next-semantic-version-cli/releases/download/v${version}/${file}"
 
 echo "Downloading ${url} with curl"
 
-curl -L "${url}" | tar xvz next-semantic-version-cli
-exe="$(pwd)/next-semantic-version-cli"
+curl -L "${url}" | tar xvz nsv
+exe="$(pwd)/nsv"
 
 echo "Downloaded and unarchived executable: ${exe}"
 
